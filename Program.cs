404ba5f7e207432181148ca8734f45112cf2ac64
Bin Zhai <zhaibin@outlook.com>
2024-05-07 17:30:33 +0800

    使用命名互斥锁,确保在同一个用户会话中不会启动多个实例

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,12 +12,27 @@ namespace _92CloudWallpaper
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
+        // 唯一的Mutex名称
+        private static string mutexName = "UniqueAppNameMutex";
+        private static Mutex mutex;
+
         [STAThread]
         static void Main()
         {
+            bool createdNew;
+            mutex = new Mutex(true, mutexName, out createdNew);
+
+            if (!createdNew)
+            {
+                // 如果Mutex已经被创建，说明已有实例在运行，显示提示并退出
+                MessageBox.Show("应用程序已在运行中。在右下角托盘可以找到我。");
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // 释放Mutex
+            mutex.ReleaseMutex();
         }
     }
 }
